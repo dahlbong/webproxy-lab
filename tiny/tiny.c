3a756ffb6b63777db867600b2f12821c5304c50c
dahyun <halover111@gmail.com>
2024-10-27 14:30:17 +0000

    요청헤더 읽기 함수 구현

--- tiny/tiny.c
+++ tiny/tiny.c
@@ -82,7 +82,13 @@ void doit(int fd) {
 }
 
 void read_requesthdrs(rio_t *rp) {
-
+  char buf[MAXLINE];
+  Rio_readlineb(rp, buf, MAXLINE);
+  while(strcmp(buf, "\r\n")) {
+    Rio_readlineb(rp, buf, MAXLINE);
+    printf("%s", buf);
+  }
+  return;
 }
 
 int parse_uri(char *uri, char *filename, char *cgiargs) {
