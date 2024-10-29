b7d6601cbcd731c78120bd4a7c2f2bc7750628cb
dahyun <halover111@gmail.com>
2024-10-29 10:45:29 +0000

    [REAC] 디버깅문 추가

--- proxy.c
+++ proxy.c
@@ -14,6 +14,9 @@ int parse_uri(char *uri, char *hostname, char *path, char *port);
 void clientError(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg);
 
 int main(int argc, char *argv[]) {
+
+    setbuf(stdout, NULL);
+
     int listenfd, connfd;
     char hostname[MAXLINE], port[MAXLINE];
     socklen_t clientlen;
@@ -36,104 +39,130 @@ int main(int argc, char *argv[]) {
 }
 
 void doit(int clientfd) {
-    int serverfd;
-    char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
-    char hostname[MAXLINE], path[MAXLINE], port[MAXLINE];
-    rio_t clientRio, serverRio;
+  int serverfd;
+  char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
+  char hostname[MAXLINE], path[MAXLINE], port[MAXLINE];
+  rio_t clientRio, serverRio;
 
-    Rio_readinitb(&clientRio, clientfd);
-    if (!Rio_readlineb(&clientRio, buf, MAXLINE))
-        return;
+  printf("\n<<<< New Client Request >>>>\n");
+  Rio_readinitb(&clientRio, clientfd);
+  if (!Rio_readlineb(&clientRio, buf, MAXLINE))
+      return;
 
-    sscanf(buf, "%s %s %s", method, uri, version);
+  printf("Client Request Line: %s", buf);
+  sscanf(buf, "%s %s %s", method, uri, version);
+  printf("Method: %s, URI: %s, Version: %s\n", method, uri, version);
 
-    if (strcasecmp(method, "GET") != 0 && strcasecmp(method, "HEAD") != 0) {
-        clientError(clientfd, method, "501", "Not implemented", "Proxy does not implement this method");
-        return;
-    }
+  if (strcasecmp(method, "GET") != 0 && strcasecmp(method, "HEAD") != 0) {
+      clientError(clientfd, method, "501", "Not implemented", "Proxy does not implement this method");
+      return;
+  }
 
-    if (parse_uri(uri, hostname, path, port) < 0) {
-        clientError(clientfd, uri, "400", "Bad request", "Proxy could not parse URI");
-        return;
-    }
+  if (parse_uri(uri, hostname, path, port) < 0) {
+      clientError(clientfd, uri, "400", "Bad request", "Proxy could not parse URI");
+      return;
+  }
 
-    serverfd = Open_clientfd(hostname, port);
-    if (serverfd < 0) {
-        clientError(clientfd, hostname, "404", "Not found", "Could not connect to server");
-        return;
-    }
+  printf("Connecting to server: %s:%s\n", hostname, port);
+  serverfd = Open_clientfd(hostname, port);
+  if (serverfd < 0) {
+      clientError(clientfd, hostname, "404", "Not found", "Could not connect to server");
+      return;
+  }
 
-    Rio_readinitb(&serverRio, serverfd);
-    doRequest(serverfd, method, path, hostname);
-    doResponse(serverfd, clientfd);
+  Rio_readinitb(&serverRio, serverfd);
+  doRequest(serverfd, method, path, hostname);
+  doResponse(serverfd, clientfd);
 
-    Close(serverfd);
+  Close(serverfd);
 }
 
 int parse_uri(char *uri, char *hostname, char *path, char *port) {
-    strcpy(port, "80");
-    char *hoststart = strstr(uri, "://") ? strstr(uri, "://") + 3 : uri;
-    char *pathstart = strchr(hoststart, '/');
+  strcpy(port, "80");
+  char *hoststart = strstr(uri, "://") ? strstr(uri, "://") + 3 : uri;
+  char *pathstart = strchr(hoststart, '/');
 
-    if (pathstart) {
-        strcpy(path, pathstart);
-        *pathstart = '\0';
-    } else
-        strcpy(path, "/");
+  if (pathstart) {
+      strcpy(path, pathstart);
+      *pathstart = '\0';
+  } else
+      strcpy(path, "/");
 
-    char *portstart = strchr(hoststart, ':');
-    if (portstart) {
-        *portstart = '\0';
-        strcpy(port, portstart + 1);
-    }
-    strcpy(hostname, hoststart);
-    return 0;
+  char *portstart = strchr(hoststart, ':');
+  if (portstart) {
+      *portstart = '\0';
+      strcpy(port, portstart + 1);
+  }
+  strcpy(hostname, hoststart);
+  return 0;
 }
 
 void doRequest(int serverfd, char *method, char *path, char *hostname) {
-    char buf[MAXLINE];
+  char buf[MAXLINE];
 
-    sprintf(buf, "%s %s HTTP/1.0\r\n", method, path);
-    Rio_writen(serverfd, buf, strlen(buf));
+  printf("\n<<<< Proxy Request to Server >>>>\n");
 
-    sprintf(buf, "Host: %s\r\n", hostname);
-    Rio_writen(serverfd, buf, strlen(buf));
+  sprintf(buf, "%s %s HTTP/1.0\r\n", method, path);
+  printf("Request Line: %s", buf);
+  Rio_writen(serverfd, buf, strlen(buf));
 
-    sprintf(buf, "%s", user_agent_hdr);
-    Rio_writen(serverfd, buf, strlen(buf));
+  sprintf(buf, "Host: %s\r\n", hostname);
+  printf("<HEADER>\nHost: %s", buf);
+  Rio_writen(serverfd, buf, strlen(buf));
 
-    sprintf(buf, "Connection: close\r\n");
-    Rio_writen(serverfd, buf, strlen(buf));
+  sprintf(buf, "%s", user_agent_hdr);
+  printf("User-Agent: %s", buf);
+  Rio_writen(serverfd, buf, strlen(buf));
 
-    sprintf(buf, "Proxy-Connection: close\r\n\r\n");
-    Rio_writen(serverfd, buf, strlen(buf));
+  sprintf(buf, "Connection: close\r\n");
+  printf("Connection: %s", buf);
+  Rio_writen(serverfd, buf, strlen(buf));
+
+  sprintf(buf, "Proxy-Connection: close\r\n\r\n");
+  printf("Proxy-Connection: %s", buf);
+  Rio_writen(serverfd, buf, strlen(buf));
 }
 
 void doResponse(int serverfd, int clientfd) {
-    char buf[MAXLINE];
-    rio_t rio;
-    ssize_t n;
+  char buf[MAXLINE];
+  rio_t rio;
+  ssize_t n;
+  int totalBytes = 0, headerEnd = 0;
 
-    Rio_readinitb(&rio, serverfd);
-    while ((n = Rio_readlineb(&rio, buf, MAXLINE)) != 0) {
-        Rio_writen(clientfd, buf, n);
+  printf("\n<<<< Server Response >>>>\n");
+  Rio_readinitb(&rio, serverfd);
+
+  while ((n = Rio_readlineb(&rio, buf, MAXLINE)) != 0) {
+    printf("Received: %s", buf);
+    Rio_writen(clientfd, buf, n);
+    totalBytes += n;
+
+    if (strcmp(buf, "\r\n") == 0) {
+      headerEnd = 1;
+      break;
     }
+  }
+  while ((n = Rio_readlineb(&rio, buf, MAXLINE)) != 0) {
+    Rio_writen(clientfd, buf, n);
+    totalBytes += n;
+  }
+  printf("<<<< Response Complete >>>>\r\n");
 }
 
 void clientError(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg) {
-    char buf[MAXLINE], body[MAXBUF];
+  char buf[MAXLINE], body[MAXBUF];
 
-    sprintf(body, "<html><title>Proxy Error</title>");
-    sprintf(body, "%s<body bgcolor=\"ffffff\">\r\n", body);
-    sprintf(body, "%s%s: %s\r\n", body, errnum, shortmsg);
-    sprintf(body, "%s<p>%s: %s\r\n", body, longmsg, cause);
-    sprintf(body, "%s<hr><em>The Proxy Web server</em>\r\n", body);
+  sprintf(body, "<html><title>Proxy Error</title>");
+  sprintf(body, "%s<body bgcolor=\"ffffff\">\r\n", body);
+  sprintf(body, "%s%s: %s\r\n", body, errnum, shortmsg);
+  sprintf(body, "%s<p>%s: %s\r\n", body, longmsg, cause);
+  sprintf(body, "%s<hr><em>The Proxy Web server</em>\r\n", body);
 
-    sprintf(buf, "HTTP/1.0 %s %s\r\n", errnum, shortmsg);
-    Rio_writen(fd, buf, strlen(buf));
-    sprintf(buf, "Content-type: text/html\r\n");
-    Rio_writen(fd, buf, strlen(buf));
-    sprintf(buf, "Content-length: %d\r\n\r\n", (int)strlen(body));
-    Rio_writen(fd, buf, strlen(buf));
-    Rio_writen(fd, body, strlen(body));
+  sprintf(buf, "HTTP/1.0 %s %s\r\n", errnum, shortmsg);
+  Rio_writen(fd, buf, strlen(buf));
+  sprintf(buf, "Content-type: text/html\r\n");
+  Rio_writen(fd, buf, strlen(buf));
+  sprintf(buf, "Content-length: %d\r\n\r\n", (int)strlen(body));
+  Rio_writen(fd, buf, strlen(buf));
+  Rio_writen(fd, body, strlen(body));
 }
